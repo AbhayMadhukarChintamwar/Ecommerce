e52f3eedbacbb0e458df5105f5e60698919981e7
Abhay218 <120081009+AbhayMadhukarChintamwar@users.noreply.github.com>
2023-10-27 10:47:35 +0530

    Create User SignIn and SignUp functionallity in  backend api

--- ecomerce-web/api/index.js
+++ ecomerce-web/api/index.js
@@ -1,22 +1,43 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-dotenv.config();  
- 
-
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
+dotenv.config()
 mongoose
-  .connect("mongodb+srv://abhaychintamwar85:<password>@mycartshops.psibzhw.mongodb.net/?retryWrites=true&w=majority")
-  .then(() => {
-    console.log('Connected to MongoDB!');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .connect(process.env.MONGODB
+    )
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
 
 const app = express();
-// app.use(express.json());
+app.use(express.json());
 
-app.listen(4000, ()=>{
-    console.log('Server is running on port 4000!')
-})  
+app.use(cookieParser());
 
+
+app.listen(4000, () => {
+    console.log('Server is running on port 3000!');
+});
+
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
